c692c76207f49741e980158443ab2f74e05ba9b4
Brad Hilton <brad.hilton.nw@gmail.com>
2023-05-05 12:33:12 -0600

    chore: Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Notebook Server Extension for Visual Studio Code
+
+This extension, "notebook-server", launches a server for getting the contents of the active notebook in Visual Studio Code.
+
+## Features
+
+The Notebook Server extension provides an HTTP server that serves the contents of the active notebook in Visual Studio Code. This can be useful for external tools or services that need to access the notebook data.
+
+## Requirements
+
+This extension requires Visual Studio Code version 1.78.0 or higher.
+
+## Extension Settings
+
+This extension contributes the following setting:
+
+- `notebookServer.port`: The port on which the notebook monitoring server listens. The default value is 4903.
+
+## Known Issues
+
+There are no known issues at the moment. If you encounter any issues, please report them on the GitHub repository.
+
+## Release Notes
+
+### 0.0.1
+
+Initial release of the Notebook Server extension.
+
+## Contributing
+
+If you have any suggestions or want to contribute to the development of this extension, feel free to open an issue or submit a pull request on the GitHub repository.
+
+**Enjoy!**
---
+++ src/extension.ts
@@ -0,0 +1,120 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode'
+import * as express from 'express'
+import * as http from 'http'
+import { TextDecoder } from 'util'
+
+type Notebook = {
+  uri: string
+  notebookType: string
+  version: number
+  isDirty: boolean
+  isUntitled: boolean
+  isClosed: boolean
+  metadata: { [key: string]: any }
+  cellCount: number
+  cells: Cell[]
+}
+
+type Cell = {
+  index: number
+  kind: vscode.NotebookCellKind
+  document: Document
+  metadata: { [key: string]: any }
+  outputs: Output[]
+}
+
+type Document = {
+  text: string
+}
+
+type Output = {
+  items: OutputItem[]
+  metadata?: { [key: string]: any }
+}
+
+type OutputItem = {
+  mime: string
+  data: string
+}
+
+const cell = (cell: vscode.NotebookCell): Cell => ({
+  index: cell.index,
+  kind: cell.kind,
+  document: document(cell.document),
+  metadata: cell.metadata,
+  outputs: cell.outputs.map(output),
+})
+
+const document = (document: vscode.TextDocument): Document => ({
+  text: document.getText(),
+})
+
+const output = (output: vscode.NotebookCellOutput): Output => ({
+  items: output.items.map(outputItem),
+  metadata: output.metadata,
+})
+
+const outputItem = (outputItem: vscode.NotebookCellOutputItem): OutputItem => {
+  let data: string
+
+  if (
+    outputItem.mime.startsWith('text/') ||
+    outputItem.mime === 'application/json'
+  ) {
+    const decoder = new TextDecoder()
+    data = decoder.decode(outputItem.data)
+  } else {
+    data = Buffer.from(outputItem.data).toString('base64')
+  }
+
+  return {
+    mime: outputItem.mime,
+    data: data,
+  }
+}
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    'Congratulations, your extension "notebook-server" is now active!'
+  )
+
+  const app = express()
+  app.get('/notebook', (req, res) => {
+    const activeNotebook: vscode.NotebookDocument | undefined =
+      vscode.window.activeNotebookEditor?.notebook
+    if (!activeNotebook) {
+      res.status(404).send('No active notebook')
+      return
+    }
+    const notebook: Notebook = {
+      uri: activeNotebook.uri.toString(),
+      notebookType: activeNotebook.notebookType,
+      version: activeNotebook.version,
+      isDirty: activeNotebook.isDirty,
+      isUntitled: activeNotebook.isUntitled,
+      isClosed: activeNotebook.isClosed,
+      metadata: activeNotebook.metadata,
+      cellCount: activeNotebook.cellCount,
+      cells: activeNotebook.getCells().map(cell),
+    }
+    res.send(notebook)
+  })
+
+  const config = vscode.workspace.getConfiguration('notebookServer')
+  const port = config.get('port', 4903)
+
+  const server = http.createServer(app)
+  server.listen(port, () => {
+    console.log(`Notebook monitoring server listening on port ${port}`)
+  })
+  context.subscriptions.push({ dispose: server.close })
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
